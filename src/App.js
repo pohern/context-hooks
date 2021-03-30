1a0027fe914e75d9ea2dc3f642604ace8c9cea9c
pohern <ohern_p@yahoo.com>
2021-03-30 11:59:08 -0500

    add children to the themecontext provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import  ThemeContextProvider from './contexts/ThemeContext'
 import NavBar from './components/NavBar'
 import BookList from './components/BookList'
 function App() {
   return (
     <div className="App">
+      <ThemeContextProvider>
       <NavBar />
       <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
