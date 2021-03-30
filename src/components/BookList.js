ca4d75f5e0430e80ec2ca046bb96cf4bc0fe2144
pohern <ohern_p@yahoo.com>
2021-03-30 12:07:38 -0500

    update styling dynamically

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-    
+    static contextType = ThemeContext
     render() { 
+        const { isLight, light, dark } = this.context
+        const theme = isLight ? light : dark
         return (
-          <div className='book-list'>
+          <div
+            className='book-list'
+            style={{ color: theme.syntax, background: theme.bg }}
+          >
             <ul>
-              <li>Book 1</li>
-              <li>Book 2</li>
-              <li>Book 3</li>
+              <li style={{ background: theme.ui }}>Book 1</li>
+              <li style={{ background: theme.ui }}>Book 2</li>
+              <li style={{ background: theme.ui }}>Book 3</li>
             </ul>
           </div>
         );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,10 @@ import { ThemeContext } from '../contexts/ThemeContext';
 class NavBar extends Component {
     static contextType = ThemeContext;
     render() { 
+        const { isLightTheme, light, dark } = this.context
+        const theme = isLightTheme ? light : dark
         return (
-          <nav>
+          <nav style={{background: theme.ui, color: theme.syntax}}>
             <h1>Context App</h1>
             <ul>
               <li>Home</li>
