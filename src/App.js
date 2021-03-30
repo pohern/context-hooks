cf522938e24add6c7e162130ec24c295d1f1b100
pohern <ohern_p@yahoo.com>
2021-03-30 12:19:54 -0500

    add button to toggle theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import  ThemeContextProvider from './contexts/ThemeContext'
 import NavBar from './components/NavBar'
 import BookList from './components/BookList'
+import ThemeToggle from './components/ThemeToggle';
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <NavBar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
